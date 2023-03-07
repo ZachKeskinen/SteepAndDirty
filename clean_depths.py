15fd0e1152e3c3e2582fdaba9cdd74e160a60fda
Zach Keskinen <zachkeskinen@gmail.com>
2023-03-07 07:07:16 -0900

    import protect main script

--- clean_depths.py
+++ clean_depths.py
@@ -10,11 +10,19 @@ raw_dir = Path('SnowEx2021_TimeSeries_DepthTransects/raw')
 parser = {'Boise River': parse_boise, 'Senator Beck': parse_senator_beck, \
           'Fraser': parse_fraser, 'Cameron Pass': parse_cameron_pass}
 
-for loc_dir in raw_dir.glob('*'):
+if __name__ == '__main__':
+
+    for loc_dir in raw_dir.glob('*'):
     print(loc_dir)
-    if loc_dir.name != 'Cameron Pass':
-        continue
+    # if loc_dir.name != 'Cameron Pass':
+    #     continue
 
     for date_dir in loc_dir.glob('*'):
         for fp in date_dir.joinpath('Depth Transects').glob('*'):
             df = parser[loc_dir.name](fp)
+            df['date'] = pd.to_datetime(date_dir.name).strftime('%Y-%m-%d')
+            df['Location'] = loc_dir.name
+            # print(df['site'])
+            res = pd.concat([res, df], ignore_index = True)
+    
+    res.to_csv('depths.csv')
